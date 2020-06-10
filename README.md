648f2164244f8eb28cab9dc9490969b0a8d69daf
austinguish <austinguish@sjtu.edu.cn>
2020-06-10 18:26:39 +0800

    add the README

---
+++ README.md
@@ -0,0 +1,2 @@
+# GCN on tox21
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='toxicology course work',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='austinguish',
+    author_email='austinguish@sjtu.edu.cn',
+    description='Using deeplearning predict compound\'s  tox'
+)
